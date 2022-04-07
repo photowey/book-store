390b72b6761f89db11bd2e67a078460fa258cb26
photowey <photowey@gmail.com>
2022-04-07 23:19:41 +0800

    feat: format code

--- cmd/bookstore/bookstore.go
+++ cmd/bookstore/bookstore.go
@@ -1,50 +1,50 @@
 package bookstore
 
 import (
-	"context"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+    "context"
+    "log"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 
-	_ "github.com/book-store/internal/store"
-	"github.com/book-store/server"
-	"github.com/book-store/store/factory"
+    _ "github.com/book-store/internal/store"
+    "github.com/book-store/server"
+    "github.com/book-store/store/factory"
 )
 
 func Run() {
-	s, err := factory.New("mem")
-	if err != nil {
-		panic(err)
-	}
+    s, err := factory.New("mem")
+    if err != nil {
+        panic(err)
+    }
 
-	srv := server.NewBookStoreServer(":8080", s)
+    srv := server.NewBookStoreServer(":8080", s)
 
-	errChan, err := srv.ListenAndServe()
-	if err != nil {
-		log.Println("web server start failed:", err)
-		return
-	}
-	log.Println("web server start ok")
+    errChan, err := srv.ListenAndServe()
+    if err != nil {
+        log.Println("web server start failed:", err)
+        return
+    }
+    log.Println("web server start ok")
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 
-	select {
-	case err = <-errChan:
-		log.Println("web server run failed:", err)
-		return
-	case <-c:
-		log.Println("bookstore program is exiting...")
-		ctx, cf := context.WithTimeout(context.Background(), time.Second)
-		defer cf()
-		err = srv.Shutdown(ctx)
-	}
+    select {
+    case err = <-errChan:
+        log.Println("web server run failed:", err)
+        return
+    case <-c:
+        log.Println("bookstore program is exiting...")
+        ctx, cf := context.WithTimeout(context.Background(), time.Second)
+        defer cf()
+        err = srv.Shutdown(ctx)
+    }
 
-	if err != nil {
-		log.Println("bookstore program exit error:", err)
-		return
-	}
-	log.Println("bookstore program exit ok")
+    if err != nil {
+        log.Println("bookstore program exit error:", err)
+        return
+    }
+    log.Println("bookstore program exit ok")
 }
--- internal/store/memstore.go
+++ internal/store/memstore.go
@@ -1,101 +1,101 @@
 package store
 
 import (
-	"sync"
+    "sync"
 
-	storeStruct "github.com/book-store/store"
-	"github.com/book-store/store/factory"
+    storeStruct "github.com/book-store/store"
+    "github.com/book-store/store/factory"
 )
 
 func init() {
-	factory.Register("mem", &MemStore{
-		books: make(map[string]*storeStruct.Book),
-	})
+    factory.Register("mem", &MemStore{
+        books: make(map[string]*storeStruct.Book),
+    })
 }
 
 type MemStore struct {
-	sync.RWMutex
-	books map[string]*storeStruct.Book
+    sync.RWMutex
+    books map[string]*storeStruct.Book
 }
 
 // Create creates a new Book in the store.
 func (ms *MemStore) Create(book *storeStruct.Book) error {
-	ms.Lock()
-	defer ms.Unlock()
+    ms.Lock()
+    defer ms.Unlock()
 
-	if _, ok := ms.books[book.Id]; ok {
-		return storeStruct.ErrExist
-	}
+    if _, ok := ms.books[book.Id]; ok {
+        return storeStruct.ErrExist
+    }
 
-	nBook := *book
-	ms.books[book.Id] = &nBook
+    nBook := *book
+    ms.books[book.Id] = &nBook
 
-	return nil
+    return nil
 }
 
 // Update updates the existed Book in the store.
 func (ms *MemStore) Update(book *storeStruct.Book) error {
-	ms.Lock()
-	defer ms.Unlock()
+    ms.Lock()
+    defer ms.Unlock()
 
-	oldBook, ok := ms.books[book.Id]
-	if !ok {
-		return storeStruct.ErrNotFound
-	}
+    oldBook, ok := ms.books[book.Id]
+    if !ok {
+        return storeStruct.ErrNotFound
+    }
 
-	nBook := *oldBook
-	if book.Name != "" {
-		nBook.Name = book.Name
-	}
+    nBook := *oldBook
+    if book.Name != "" {
+        nBook.Name = book.Name
+    }
 
-	if book.Authors != nil {
-		nBook.Authors = book.Authors
-	}
+    if book.Authors != nil {
+        nBook.Authors = book.Authors
+    }
 
-	if book.Press != "" {
-		nBook.Press = book.Press
-	}
+    if book.Press != "" {
+        nBook.Press = book.Press
+    }
 
-	ms.books[book.Id] = &nBook
+    ms.books[book.Id] = &nBook
 
-	return nil
+    return nil
 }
 
 // Get retrieves a book from the store, by id. If no such id exists. an
 // error is returned.
 func (ms *MemStore) Get(id string) (storeStruct.Book, error) {
-	ms.RLock()
-	defer ms.RUnlock()
+    ms.RLock()
+    defer ms.RUnlock()
 
-	t, ok := ms.books[id]
-	if ok {
-		return *t, nil
-	}
-	return storeStruct.Book{}, storeStruct.ErrNotFound
+    t, ok := ms.books[id]
+    if ok {
+        return *t, nil
+    }
+    return storeStruct.Book{}, storeStruct.ErrNotFound
 }
 
 // Delete deletes the book with the given id. If no such id exist. an error
 // is returned.
 func (ms *MemStore) Delete(id string) error {
-	ms.Lock()
-	defer ms.Unlock()
+    ms.Lock()
+    defer ms.Unlock()
 
-	if _, ok := ms.books[id]; !ok {
-		return storeStruct.ErrNotFound
-	}
+    if _, ok := ms.books[id]; !ok {
+        return storeStruct.ErrNotFound
+    }
 
-	delete(ms.books, id)
-	return nil
+    delete(ms.books, id)
+    return nil
 }
 
 // GetAll returns all the books in the store, in arbitrary order.
 func (ms *MemStore) GetAll() ([]storeStruct.Book, error) {
-	ms.RLock()
-	defer ms.RUnlock()
+    ms.RLock()
+    defer ms.RUnlock()
 
-	allBooks := make([]storeStruct.Book, 0, len(ms.books))
-	for _, book := range ms.books {
-		allBooks = append(allBooks, *book)
-	}
-	return allBooks, nil
+    allBooks := make([]storeStruct.Book, 0, len(ms.books))
+    for _, book := range ms.books {
+        allBooks = append(allBooks, *book)
+    }
+    return allBooks, nil
 }
